de11d1f378e1c3d6a225d15fdb7018775df4f2dc
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2020-02-04 18:37:08 -0500

    duh.

--- cannon/cannon_testsample.sh
+++ cannon/cannon_testsample.sh
@@ -6,9 +6,9 @@
 #SBATCH --mem-per-cpu=1000 # Memory per node in MB (see also --mem-per-cpu)
 #SBATCH -p gpu # Partition to submit to
 #SBATCH -t 06:00:00 # Runtime
-#SBATCH -J force_run_test
-#SBATCH -o /n/scratchlfs02/eisenstein_lab/bdjohnson/jades_force/cannon/logs/runtest_%A_%a.out # Standard out goes to this file
-#SBATCH -e /n/scratchlfs02/eisenstein_lab/bdjohnson/jades_force/cannon/logs/runtest_%A_%a.err # Standard err goes to this file
+#SBATCH -J force_sample_test
+#SBATCH -o /n/holyscratch01/eisenstein_lab/bdjohnson/jades_force/cannon/logs/runtest_%A_%a.out # Standard out goes to this file
+#SBATCH -e /n/holyscratch01/eisenstein_lab/bdjohnson/jades_force/cannon/logs/runtest_%A_%a.err # Standard err goes to this file
 
 module purge
 module load intel/19.0.5-fasrc01 openmpi/4.0.1-fasrc01 hdf5/1.10.5-fasrc01
@@ -16,8 +16,8 @@ module load cuda/10.1.243-fasrc01
 module load Anaconda3/5.0.1-fasrc01
 
 # where code will be run
-jdir=/n/scratchlfs02/eisenstein_lab/bdjohnson/jades_force/cannon/
+jdir=${SCRATCH}/eisenstein_lab/bdjohnson/jades_force/cannon/
 
 source activate jadespho
 cd $jdir
-python test_run.py
+python test_sample.py --logging --seed_index=603 --outfile=test_sample_id603.h5
