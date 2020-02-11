fix off by one error in crpix; better plotting of patch data.

src/catalog.py
@@ -33,7 +33,7 @@ def sourcecat_dtype(source_type=np.float64, bands=None):
     dt = [(t, np.int32) for t in tags]
     dt += [(c, source_type)
            for c in SHAPE_COLS]
-    dt += [(c, source_type, nband)
+    dt += [(c, source_type, (nband,))
            for c in [FLUX_COL]]
     return np.dtype(dt)
 
@@ -92,7 +92,7 @@ def catalog_to_scene(sourcepars, band_ids, filters,
     for ii, pars in enumerate(sourcepars):
         x, y, q, pa, n, rh = [pars[f] for f in SHAPE_COLS]
         gid = pars["id"]
-        flux = pars[FLUX_COL]
+        flux = np.atleast_1d(pars[FLUX_COL])
         s = Galaxy(filters=filters, splinedata=splinedata,
                    free_sersic=free_sersic)
         s.global_id = gid

src/show_patch.py
@@ -23,9 +23,9 @@ def show_exp(xpix, ypix, value, ax=None, **imshow_kwargs):
     standard astro format (x increasing left to right, y increasing bottom to
     top)
     """
-    lo = np.array((xpix.min(), ypix.min()))
-    hi = np.array((xpix.max(), ypix.max()))
-    size = hi - lo + 1
+    lo = np.array((xpix.min(), ypix.min())) - 0.5
+    hi = np.array((xpix.max(), ypix.max())) + 0.5
+    size = hi - lo
     im = np.zeros(size.astype(int)) + np.nan
 
     x = (xpix-lo[0]).astype(int)
@@ -39,10 +39,17 @@ def show_exp(xpix, ypix, value, ax=None, **imshow_kwargs):
     return ax
 
 
-def sky_to_pix(ra, dec, group, ref_coords=0.):
-    crval = group["crval"][:]
-    crpix = group["crpix"][:]
-    CW = group["CW"][:]
+def sky_to_pix(ra, dec, group=None, patch=None, exp_idx=0, ref_coords=0.):
+
+    if group:
+        crval = group["crval"][:]
+        crpix = group["crpix"][:]
+        CW = group["CW"][:]
+    elif patch:
+        crval = patch.crval[exp_idx]
+        crpix = patch.crpix[exp_idx]
+        CW = patch.CW[exp_idx]
+        ref_coords = patch.patch_reference_coordinates
 
     if len(CW) != len(ra):
         CW = CW[0]